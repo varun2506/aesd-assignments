8febc1479a874f4c22a6c8680748c099a5979070
Dan Walkes <danwalkes@trellis-logic.com>
2019-10-29 19:47:22 -0600

    Add debug prints

--- aesd-char-driver/aesdchar.c
+++ aesd-char-driver/aesdchar.c
@@ -28,6 +28,7 @@ struct aesd_dev aesd_device;
 
 int aesd_open(struct inode *inode, struct file *filp)
 {
+	PDEBUG("open");
 	/**
 	 * TODO: handle open
 	 */
@@ -36,6 +37,7 @@ int aesd_open(struct inode *inode, struct file *filp)
 
 int aesd_release(struct inode *inode, struct file *filp)
 {
+	PDEBUG("release");
 	/**
 	 * TODO: handle release
 	 */
@@ -46,6 +48,7 @@ ssize_t aesd_read(struct file *filp, char __user *buf, size_t count,
                 loff_t *f_pos)
 {
 	ssize_t retval = 0;
+	PDEBUG("read %zu bytes with offset %lld",count,*f_pos);
 	/**
 	 * TODO: handle read
 	 */
@@ -56,6 +59,7 @@ ssize_t aesd_write(struct file *filp, const char __user *buf, size_t count,
                 loff_t *f_pos)
 {
 	ssize_t retval = -ENOMEM;
+	PDEBUG("write %zu bytes with offset %lld",count,*f_pos);
 	/**
 	 * TODO: handle write
 	 */
