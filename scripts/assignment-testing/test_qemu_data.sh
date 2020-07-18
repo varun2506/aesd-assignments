d6bb9a744c9514244c3732af7864f1e23e679ccd
Akshita Bhasin <akshita.bhasin@colorado.edu>
2020-07-18 14:34:29 -0600

    Starter code for assignment 4

---
+++ scripts/assignment-testing/test_qemu_data.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+
+source script-helpers
+source assignment-timeout
+
+read_qemu "This architecture does not have kernel memory protection" >/dev/null 2>&1
+#Prints obtained between these read_qemu functions would be validation check prints inside qemu
+read_qemu "Inside QEMU: Exiting with"
+exit 0
