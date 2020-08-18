ad8c145c4d871497419596bef64c445a8f45c79c
Akshita Bhasin <akshita.bhasin@colorado.edu>
2020-08-18 12:25:39 -0600

    Remove extra files

--- scripts/testall.sh
+++
@@ -1,5 +0,0 @@
-cd `dirname $0`
-# See https://docs.gitlab.com/ee/gitlab-basics/create-project.html#push-to-create-a-new-project
-# and place all repositories under the cu-ecen-aesd-student-assignments group on gitlab
-# Specify the directory above this one as the base for overlay files
-./github-classroom-scripts/prsetup.py --create_local --overlay_files .. --push_alternate git@gitlab.com:cu-ecen-aesd-student-assignments "$@"
