210ad67d654d1386b42f148f3f6116755ed2c600
Dan Walkes <danwalkes@trellis-logic.com>
2021-05-31 11:33:45 -0600

    Update manual linux scripts and instructions

--- finder-app/manual-linux.sh
+++ finder-app/manual-linux.sh
@@ -5,61 +5,76 @@
 set -e
 set -u
 
-
-OUTDIR=/tmp/aesd
-REPO=git://git.kernel.org/pub/scm/linux/kernel/git/stable/linux-stable.git
-#DIR is the path where writer executable, finder.sh and tester.sh are stored.
-#Refer: https://www.electrictoolbox.com/bash-script-directory/
-DIR="$( cd "$(dirname "$0")" ; pwd -P )"
-
-
+OUTDIR=/tmp/aeld
+KERNEL_REPO=git://git.kernel.org/pub/scm/linux/kernel/git/stable/linux-stable.git
+KERNEL_VERSION=v5.1.10
+BUILDROOT_VERSION=1_33_1
+FINDER_APP_DIR=$(realpath $(dirname $0))
+ARCH=arm64
+CROSS_COMPILE=aarch64-none-linux-gnu-
 
 if [ $# -lt 1 ]
 then
-	echo -e "\nUSING DEFAULT DIRECTORY ${OUTDIR} TO STORE FILES\n"
+	echo "Using default directory ${OUTDIR} for output"
 else
 	OUTDIR=$1
-	echo -e "\n USING ${OUTDIR} DIRECTORY TO STORE FILES\n"
+	echo "Using passed directory ${OUTDIR} for output"
 fi
 
-if [ -d "$OUTDIR" ]
+mkdir -p ${OUTDIR}
+
+cd "$OUTDIR"
+if [ ! -d "${OUTDIR}/linux-stable" ]; then
+    #Clone only if the repository does not exist.
+	echo "CLONING GIT LINUX STABLE VERSION ${KERNEL_VERSION} IN ${OUTDIR}"
+	git clone ${KERNEL_REPO} --depth 1 --single-branch --branch ${KERNEL_VERSION}
+fi
+if [ ! -e ${OUTDIR}/linux-stable/arch/${ARCH}/boot/Image ]; then
+    cd linux-stable
+    echo "Checking out version ${KERNEL_VERSION}"
+    git checkout ${KERNEL_VERSION}
+
+    # TODO: Add your kernel build steps here
+fi 
+
+echo "Adding the Image in outdir"
+
+echo "Creating the staging directory for the root filesystem"
+cd "$OUTDIR"
+if [ -d "${OUTDIR}/rootfs" ]
 then
-	echo -e "\nDIRECTORY ALREADY EXISTS\n"
-#	rm -rf "$OUTDIR"/*
-else
-	mkdir -p "$OUTDIR"
-	
-	if [ -d "$OUTDIR" ]
-	then
-		echo -e "\n${OUTDIR} CREATED\n"
-	else
-		echo -e "\nCANNOT CREATE ${OUTDIR}\n"
-	exit 1
-	fi
+	echo "Deleting rootfs directory at ${OUTDIR}/rootfs and starting over"
+    sudo rm  -rf ${OUTDIR}/rootfs
 fi
 
+# TODO: Create necessary base directories
 
+cd "$OUTDIR"
+if [ ! -d "${OUTDIR}/busybox" ]
+then
+	git clone git://busybox.net/busybox.git
+    cd busybox
+    git checkout ${BUSYBOX_VERSION}
+    # TODO:  Configure busybox
+else
+    cd busybox
+fi
 
-#Installing Dependencies
-echo -e "\nINSTALLING THE REQUIRED DEPENDENCIES FOR THE INSTALLATION\n"
-"${DIR}"/dependencies.sh
+# TODO: Make and insatll busybox
 
+echo "Library dependencies"
+${CROSS_COMPILE}readelf -a bin/busybox | grep "program interpreter"
+${CROSS_COMPILE}readelf -a bin/busybox | grep "Shared library"
 
-#TODO: Call a script to perform this function here or place a logic inline
-#Installing Linux Kernel
-echo -e "\nINSTALLING LINUX KERNEL\n"
+# TODO: Add library dependencies to rootfs
 
+# TODO: Make device nodes
 
-#TODO: Call a script to perform this function here or place a logic inline
-#Installing Rootfs using Busybox
-echo -e "\nINSTALLING ROOTFS USING BUSYBOX\n"
+# TODO: Clean and build the writer utility
 
+# TODO: Copy the finder related scripts and executables to the /home directory
+# on the target rootfs
 
-#TODO: Call a script to perform this function here or place a logic inline
-#Copying writer executable, tester.sh and finder.sh here
-echo -e "\nCOPYING WRITER EXECUTABLE, TESTER.SH AND FINDER.SH TO ${OUTDIR}/rootfs/bin\n"
+# TODO: Chown the root directory
 
-
-#TODO: Call a script to perform this function here or place a logic inline
-#Creating Standalone initramfs.
-echo -e "\nCREATING STANDALONE INITRAMFS\n"
+# TODO: Create initramfs.cpio.gz
