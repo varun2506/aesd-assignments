ff37b245a7ffc813341f75e06973e7dfa50fa30a
Dan Walkes <daniel.walkes@colorado.edu>
2021-09-08 17:35:37 -0600

    Fix typo

--- examples/systemcalls/systemcalls.c
+++ examples/systemcalls/systemcalls.c
@@ -12,7 +12,7 @@ bool do_system(const char *cmd)
 
 /*
  * TODO  add your code here
- *  Call the system() function with the command set in the dmd
+ *  Call the system() function with the command set in the cmd
  *   and return a boolean true if the system() call completed with success 
  *   or false() if it returned a failure
 */
